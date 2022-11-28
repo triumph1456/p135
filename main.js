94220e42f516ef8935385d8996752ede0448bb28
triumph1456 <102103383+triumph1456@users.noreply.github.com>
2022-11-28 18:25:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup() {
+    video = createCapture(VIDEO); 
+    video.size(550, 500); 
+    
+    
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  }
+  
+  function modelLoaded() {
+      console.log('poseNet is Initialized!');
+  }
+  
+  
+  function draw() {
+      background('#969A97');
+  }
